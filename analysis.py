0649bfa7e38afa0e367e5b7eb368fda2dbc32ef8
Hariharan <hariharan.m2716@gmail.com>
2026-03-20 05:00:45 +0530

    Using built in functions to Etract and Analyse Montly sales trend and so...

--- analysis.py
+++ analysis.py
@@ -10,3 +10,14 @@ df['Order_Data'] = pd.to_datetime(df['Order_Date'])
 # Removing the missing values
 df = df.dropna()
 # Remove missing values. See the User Guide <missing_data> for more on which values are considered missing, and how to work with missing data.
+
+# Extract month name
+df['Month'] = df['Order_Date'].dt.month_name()
+# Analysis
+# Sales by Product
+product_sales = df.groupby('Product')['Sales'].sum().sort_values(ascending=False)
+
+# Monthly Sales Trend, Region-wise Sales
+monthly_sales = df.groupby('Month')['Sales'].sum()
+region_sales = df.groupby('Region')['Sales'].sum().sort_values(ascending=False)
+top_orders = df.sort_values(by='Sales', ascending=False).head(3)
